6d27aaa30709125fbfbc28fb762c910fe763582e
INGRID <ingridcarolinelima@hotmail.com>
2020-04-21 20:03:14 -0300

    pagina startMenu em desenvolvimento 2

--- src/pages/StartMenu.tsx
+++ src/pages/StartMenu.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import StartMenuCard from '../components/StartMenuCard'
+import { Redirect } from 'react-router-dom';
 
-const StartMenu = () => {
-    return (
-        <div>
-            <h1>PAGE TO BUILD! - StartMenu</h1>
-        </div>
-    )
+
+export default class StartMenu extends Component {
+    state = {
+        gotoDietPage: false,
+        gotoTraningPage:false,
+        gotoNutriPage:false,
+        gotoMeasuresPage:false
+    };
+
+    onUserHandleChange = (event: any) => {
+        this.setState({ user: event.target.value });
+        console.log(this.state)
+    }
+
+    onSubmit = () => {
+        this.setState({ gotoStartPage: true });
+    }
+
+    render() {
+        if (this.state.gotoDietPage) {
+            return <Redirect to="/Diet" />
+        }else if(this.state.gotoMeasuresPage){
+            return <Redirect to="/Measures" />
+        }else if(this.state.gotoTraningPage){
+            return <Redirect to="/Traning" />
+        }else if(this.state.gotoNutriPage){
+            return <Redirect to="/Nutricionist" />
+        }
+        return (
+                <StartMenuCard onSubmit={this.onSubmit} />
+        )
+    }
 }
-
-export default StartMenu;
