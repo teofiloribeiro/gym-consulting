42904018d19441b514c614ce203edf4bcb78153e
Danilo Alves <danilok16alves@hotmail.com>
2020-04-21 22:56:53 -0300

    Ajuste no StartMenuCard.tsx

--- src/components/StartMenuCard.tsx
+++ src/components/StartMenuCard.tsx
@@ -9,12 +9,23 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 
+import dietImg from './assets/diet.jpg';
+import traningImg from './assets/training.jpg';
+import nutricionistaImg from './assets/nutricionista.jpg';
+import medidaImg from './assets/medidas.jpg';
+
+
+import "./StartMenuCard.scss";
+
 const useStyle = makeStyles({
     root: {
         maxWidth: 300,
         flexGrow: 1,
-        marginRight: 0
 
+
+        marginRight: 0,
+        maxHeight: 400,
+        minHeight: 400
     },
     botton: {
         marginBottom: 5,
@@ -25,22 +36,26 @@ const useStyle = makeStyles({
 const StartMenuCard = (props: any) => {
     const classes = useStyle();
     return (
-    
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+        >
             <Grid item xs={3} md={6} lg={4}>
                 <Card className={classes.root}>
                     <CardActionArea>
                         <CardMedia
                             component="img"
-                            image={props.img}
-                            title={props.title}
+                            image={dietImg}
+                            title="Treino"
                         />
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="p">
-                                {props.title}
-                            </Typography >
-
+                                Dieta
+                    </Typography >
                             <Typography gutterBottom variant="body2" color="textSecondary" component="p">
-                                {props.description}
+                                Uma boa dieta e treinos trazem melhores resultados, além de fazer bem a saúde.
                     </Typography>
                         </CardContent>
                     </CardActionArea>
@@ -50,9 +65,125 @@ const StartMenuCard = (props: any) => {
                 </Button>
                     </CardActions>
                 </Card>
-            </Grid> 
+            </Grid> <br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <Grid item xs={3} md={6} lg={4}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image={traningImg}
+                            title="Treino"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="p">
+                                Treino
+                    </Typography >
+        <Card className={classes.root}>
+            <CardActionArea   onClick={props.onSubmit}>
+                <CardContent>
+                    <CardMedia className="imgSize"
+                        component="img"
+                        image={props.img}
+                        title={props.title}
+                    />
+                    <Typography gutterBottom variant="h5" component="p">
+                        {props.title}
+                    </Typography >
+                            <Typography gutterBottom variant="body2" color="textSecondary" component="p">
+                                Os treinos são essenciais para a saúde,perda de gordura e o guanho de massa magra.
+                    </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" onClick={props.onSubmit}>
+                            Acessar
+                </Button>
+                    </CardActions>
+                </Card>
+            </Grid> <br />
+            <Grid item xs={3} md={6} lg={4}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image={nutricionistaImg}
+                            title="Nutritionista"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="p">
+                                Nutricionista
+                    </Typography >
+                            <Typography gutterBottom variant="body2" color="textSecondary" component="p">
+                                O nutricionista elabora uma dieta que atenda a sua necessidade. Para resultados mais rápido e seguro.
+                    </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" onClick={props.onSubmit}>
+                            Acessar
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+
+            <Grid
+                container
+                justify="flex-start"
+                alignItems="flex-end"
+            >
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image={medidaImg}
+                            title="Medidas"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="p">
+                                Medidas
+                    </Typography >
+                            <Typography gutterBottom variant="body2" color="textSecondary" component="p">
+                                Para saber sua evolução, é importante saber as suas medidas e seu peso.
+                    </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" onClick={props.onSubmit}>
+                            Acessar
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </Grid>
+
+                    <Typography gutterBottom variant="body2" color="textSecondary" component="p">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+
+
+
+
+
     );
 }
+
 
 
 export default StartMenuCard;
