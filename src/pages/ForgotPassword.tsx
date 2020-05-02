d39475d1742f52fd52df32a0409e21c1d20f465a
pedro96h <pedro96h@gmail.com>
2020-05-01 21:28:03 -0300

    layout

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { Card , Typography} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import SubdirectoryArrowLeftIcon from '@material-ui/icons/SubdirectoryArrowLeft';
+
+
+import CancelIcon from '@material-ui/icons/Cancel';
+
+
+    
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
 const ForgotPassword = () => {
     return (
-        <div>
-            <h1>TO BUILD! - FORGOT PASSWORD</h1>
-        </div>
+        <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                className="cardContainer">
+                <Grid item xs={12} md={6} lg={5} >
+                    <Card>
+                        <div style={{margin:"40px"}}>
+                            <Typography variant="h4" component="h2" align="center">
+                                Recuperando senha
+                            </Typography>
+                            
+                        </div>
+                        <div style={{display:"flex" , flexDirection:"column"}}>
+                            <TextField id="user_field" label="Digite o nome do seu usuário"  variant="outlined" style={{margin:"5px 50px 5px"}} />
+                            <TextField id="email_field" label="Digite seu email"  variant="outlined" style={{margin:"5px 50px 5px"}}/>
+                        </div>
+                        <div style={{display:"center", margin:"10px 50px 20px"}}>
+                        <Button variant="contained" endIcon={<SubdirectoryArrowLeftIcon/>}>
+                            Voltar
+                        </Button>
+                        <Button variant="contained" color="primary" style={{margin:"0px 10px 0px"}} endIcon={<SendIcon/>}>
+                            Enviar solicitação
+                        </Button>
+                        <Button variant="contained" color="secondary" endIcon={<CancelIcon/>}>
+                            Cancelar
+                        </Button>
+                        </div>
+                    </Card>
+                </Grid>
+        </Grid>
     )
 }
 
